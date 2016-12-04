6fdb29bf4c32b8d0690dc9726d98815fc57de2e0
Ryan Faulhaber <faulhaber.14@osu.edu>
2016-12-04 14:11:32 -0500

    some logic added, but classes will need to be made

--- RPN-calc/src/main.kt
+++ RPN-calc/src/main.kt
@@ -1,6 +1,28 @@
 import java.util.*
 import kotlin.system.exitProcess
 
+class Stack<Any> {
+    private var data = ArrayList<Any>()
+
+    fun push(n: Any) {
+        data.add(n)
+    }
+
+    fun pop() : Any {
+       return data.removeAt(data.lastIndex)
+    }
+
+    fun size() : Int {
+        return data.size
+    }
+}
+
+class Expression(private var data : Stack<ExpressionItem>) {
+}
+
+class ExpressionItem(var item : String) {
+}
+
 fun main(args: Array<String>) {
     var line = readLine()
 
@@ -11,26 +33,67 @@ fun main(args: Array<String>) {
             val expr = if (line != null) parseExpression(line) else break
 
             var i = 0;
-            while (expr.size > 0) {
-                if (isOperand(expr[i])) {
-                    // TODO: finish
+            var stack = Stack<Double>()
+            while (stack.size() > 0) {
+                val item = expr[i]
+                if (isOperand(item)) {
+                    if (item == "!") {
+                        stack.push(factorial(stack.pop().toInt()).toDouble())
+                    } else {
+                        val eval = eval(stack.pop(), stack.pop(), item)
+                        if (eval == null) {
+                            stack.push(0.0)
+                        } else {
+                            stack.push(eval)
+                        }
+                    }
                 } else {
-                    // TODO: finish
+                    stack.push(item.toDouble());
                 }
+
+                i++;
             }
         }
 
-        line = readLine();
+        line = readLine()
     }
 }
 
-fun parseExpression(expr: String) : ArrayList<String> {
+fun parseExpression(expr: String): ArrayList<String> {
     return ArrayList<String>(expr.split(" "))
 }
 
-fun isOperand(c: String) : Boolean {
-    when(c) {
+fun isOperand(c: String): Boolean {
+    when (c) {
         "+", "-", "*", "/", "//", "!", "%", "^" -> return true
         else -> return false
+    }
+}
+
+fun eval(a: Double, b: Double, o: String): Double? {
+    when (o) {
+        "+" -> return a + b
+        "-" -> return a - b
+        "*" -> return a * b
+        "/" -> return a / b
+        "%" -> return a % b
+        "^" -> return Math.pow(a, b)
+        else -> return null
+    }
+}
+
+fun factorial(n: Int): Int {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        var product = 0
+        var counter = n
+
+        while (counter > 0) {
+            product *= counter
+            counter--
+        }
+
+        return product
     }
 }
---
+++ RPN-calc/test/maintest.kt
@@ -0,0 +1,3 @@
+/**
+ * Created by ryan on 12/4/16.
+ */
