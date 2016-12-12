a62fb411b320ce93fa633b514e9bfe2e2ebe4cad
Ryan Faulhaber <faulhaber.14@osu.edu>
2016-12-12 15:42:38 -0500

    Starting bonus 2

--- rack/src/main/kotlin/Rack.kt
+++ rack/src/main/kotlin/Rack.kt
@@ -1,19 +1,27 @@
 package rack
 
+import java.util.*
+
 class Rack(val letters: String) {
     fun contains(word: String) : Boolean {
-        val lettersCopy = Array(letters.length, Int::toString).toMutableList()
+        val lettersCopy = Array(letters.length, { i -> letters[i].toString()}).toMutableList()
 
         for (c in word) {
             val s = c.toString()
 
             if (lettersCopy.contains(s)) {
                 lettersCopy.removeAt(lettersCopy.indexOf(s))
+            } else if (lettersCopy.contains("?")) {
+                lettersCopy.removeAt(lettersCopy.indexOf("?"))
             } else {
-                return false
+                return false;
             }
         }
 
         return true
     }
+
+//    fun findLongest(word: String) : String {
+//
+//    }
 }
--- rack/src/test/kotlin/RackTest.kt
+++ rack/src/test/kotlin/RackTest.kt
@@ -8,13 +8,24 @@ import kotlin.test.assertTrue
 class RackTest : Spek({
     given("a rack of letters") {
         val rack = Rack("ladilmy")
+        val rack2 = Rack("pizza??")
 
         it("should contain a word made up of given letters") {
             assertTrue(rack.contains("daily"))
+            assertTrue(rack.contains("my"))
+            assertTrue(rack.contains("laid"))
         }
 
         it("should not contain a word made up of letters not given") {
             assertFalse(rack.contains("program"))
+            assertFalse(rack.contains("control"))
+            assertFalse(rack.contains("gradle"))
+        }
+
+        it("should match wildcard tiles") {
+            assertTrue(rack2.contains("pizzazz"))
+            assertTrue(rack2.contains("pizza"))
+            assertFalse(rack2.contains("marathon"))
         }
     }
 })
